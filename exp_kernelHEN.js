7423c54e4b4f086d50fea8805b934bde48ef1820
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2020-09-09 12:39:24 -0300

    Add files via upload

--- exp_kernelHEN.js
+++ exp_kernelHEN.js
@@ -33775,6 +33775,6 @@ if (main_ret == 179 || main_ret == 0) {
 } 
 else {
 	localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
-	window.msgs.innerHTML="<h1 style='color:red;font-size:25px;text-align:center;'>Jailbreak FALHOU! - Reinicie o seu PS4 e tente novamente.</h1>";
+	window.msgs.innerHTML="<h1 style='color:red;font-size:25px;text-align:center;'>Jailbreak FALHOU! - Pressione o \"PS\" e reinicie o seu PS4 novamente.</h1>";
 }
 }
