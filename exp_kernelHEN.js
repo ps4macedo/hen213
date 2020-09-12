63fa520ce6cb3905786d8929e1a0bdf6cf544382
ps4macedo <69283643+ps4macedo@users.noreply.github.com>
2020-09-12 06:13:42 -0300

    Add files via upload

--- exp_kernelHEN.js
+++ exp_kernelHEN.js
@@ -33768,13 +33768,13 @@ var _ = malloc_nogc.pop();
 
 if (main_ret == 179 || main_ret == 0) {
 	localStorage.passcount = ++localStorage.passcount;window.passCounter.innerHTML=localStorage.passcount;
-	window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'><font color=\"blue\">Exploit ✔ </font><font color=\"red\">HEN ... </font></h1>";
+	window.msgs.innerHTML="<h1 style='font-size:25px;text-align:center;'><font color=\"#6089f6\">Exploit ✔ </font><font color=\"#ee596f\">HEN ... </font></h1>";
     setTimeout(function(){
     plLoader();
     },1000);
 } 
 else {
 	localStorage.failcount = ++localStorage.failcount;window.failCounter.innerHTML=localStorage.failcount;
-	window.msgs.innerHTML="<h1 style='color:red;font-size:25px;text-align:center;'>Jailbreak FALHOU! - Pressione o \"PS\" e reinicie o seu PS4 novamente.</h1>";
+	window.msgs.innerHTML="<h1 style='color:#ee596f;font-size:25px;text-align:center;'>Jailbreak FALHOU! - Pressione o \"PS\" e reinicie o seu PS4 novamente.</h1>";
 }
 }
